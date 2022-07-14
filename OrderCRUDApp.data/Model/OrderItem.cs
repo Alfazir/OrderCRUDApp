25a207b944e6a17586bff9fc707b9a5cb21360a8
azelf longo <az.elf.longo@gmail.com>
2022-07-14 14:31:22 +0300

    up Repos

---
+++ OrderCRUDApp.data/Model/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderCRUD.DAL.Entities
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public Order? Order { get; set; }
+        public string? Name { get; set; }
+        public decimal Quantity { get; set; }
+        public string? Unit { get; set; }
+
+    }
+}
